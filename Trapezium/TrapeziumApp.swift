f15c4107cdf55744a253619fcd27009f2028385a
Siddharth Pande <siddharth.v.pande@gmail.com>
2021-05-27 18:58:00 +0530

    Initial Commit

---
+++ Trapezium/TrapeziumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrapeziumApp.swift
+//  Trapezium
+//
+//  Created by Siddharth on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TrapeziumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
